b223fe62e8ebd8d414696c6f761391a5067013b6
hsien wei <hsien.w.wei@gmail.com>
2022-02-26 20:54:14 +0800

    update test case

--- js/server.js
+++ js/server.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const fs = require('fs');
-const qs = require("querystring");
+const qs = require('querystring');
+// crypto 模塊提供了加密功能，其中包括了用於 OpenSSL 散列、HMAC、加密、解密、簽名、以及驗證的函數的一整套封裝
+const crypto = require('crypto');
 
 const ip = '127.0.0.1';
 const port = 3000;
@@ -16,9 +18,9 @@ const sendRespond = (fileName, statusCode, respond) => {
             respond.setHeader('Content-Type', 'text/html');
             respond.end(`
                 <!DOCTYPE html>
-                    <html lang="en">
+                    <html lang='en'>
                     <head>
-                        <meta charset="utf-8">
+                        <meta charset='utf-8'>
                         <title>TEST</title>
                     </head>
                     <body>
@@ -46,27 +48,36 @@ const server = http.createServer((request, respond) => {
         // respond.setHeader('Expires', new Date(Date.now() + 10 * 1000).toUTCString());
 
         // 設定 cache-control
-        // res.setHeader('Cache-Control', 'max-age=20')
+        // respond.setHeader('Cache-Control', 'max-age=20')
 
         // 設定 last-modified
         // respond.setHeader('Cache-Control', 'no-cache');
         // respond.setHeader('last-modified', new Date().toUTCString());
         // if (new Date(request.headers['if-modified-since']).getTime() + 3 * 1000 > Date.now()) {
-        //     console.log('協商緩存....')
+        //     console.log('協商緩存有中....')
         //     respond.statusCode = 304;
         //     respond.end();
         //     return
         // }
 
         // 設定 etag
-
-
+        respond.setHeader('Cache-Control', 'no-cache')
+        // 透過 hash 加密產生 當作 etag 的 hash key
+        const hash = crypto.createHash('sha1').update('index.html').digest('hex');
+        respond.setHeader('Etag', hash);
+    
+        if(request.headers['if-none-match'] === hash){
+            console.log('Etag 協商有中....');
+            respond.statusCode = 304;
+            respond.end();
+            return
+        }
 
         // const {url} = req
         if ('/' === url) {
             respond.end(`
             <html>
-                <!-- <meta http-equiv="Refresh" content="5" /> -->
+                <!-- <meta http-equiv='Refresh' content='5' /> -->
                 Html Update Time: ${updateTime()}
                 <script src='main.js'></script>
             </html>
